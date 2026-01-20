e99283c4f99369b22f2a0a03c62769761db34ae2
Kelvin Kuda <104599692+Kel202@users.noreply.github.com>
2026-01-20 19:47:10 +0200

    Define location and environment variables
    Add variables for location and environment in Terraform

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,1 +1,12 @@
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "eastus"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
 
