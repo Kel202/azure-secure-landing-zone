d8c0adef42a647593d5fa5c80c30130c2f530848
Kel202 <kudakamz10@gmail.com>
2026-03-13 23:49:46 +0200

    feat: implement hub and spoke virtual network architecture

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,3 +2,65 @@ resource "azurerm_resource_group" "network_rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+#Hub VNet
+resource "azurerm_virtual_network" "hub_vnet" {
+  name                = "hub-vnet-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+#Hub Subnets
+resource "azurerm_subnet" "jump_subnet" {
+  name                 = "JumpSubnet"
+  resource_group_name  = azurerm_resource_group.network_rg.name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "firewall_subnet" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.network_rg.name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = ["10.0.0.0/26"]
+}
+
+
+#Spoke VNET
+resource "azurerm_virtual_network" "spoke_vnet" {
+  name                = "spoke-vnet-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+#Spoke Subnets
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "AppSubnet"
+  resource_group_name  = azurerm_resource_group.network_rg.name
+  virtual_network_name = azurerm_virtual_network.spoke_vnet.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "DbSubnet"
+  resource_group_name  = azurerm_resource_group.network_rg.name
+  virtual_network_name = azurerm_virtual_network.spoke_vnet.name
+  address_prefixes     = ["10.1.2.0/24"]
+}
+
+#VNet Peering
+resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+  name                      = "hub-to-spoke"
+  resource_group_name       = azurerm_resource_group.network_rg.name
+  virtual_network_name      = azurerm_virtual_network.hub_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke_vnet.id
+}
+
+resource "azurerm_virtual_network_peering" "spoke_to_hub" {
+  name                      = "spoke-to-hub"
+  resource_group_name       = azurerm_resource_group.network_rg.name
+  virtual_network_name      = azurerm_virtual_network.spoke_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_vnet.id
+}
